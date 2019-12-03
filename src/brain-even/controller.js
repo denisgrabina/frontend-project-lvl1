c61caab05cb96d16271c76f853e13bf8da272f39
Denis Grabina <denisgrabina@gmail.com>
2019-12-03 20:00:07 +0200

    logic is fully ready

--- src/brain-even/controller.js
+++ src/brain-even/controller.js
@@ -7,26 +7,32 @@ const controller = () => {
   const numbers = [15, 6, 7];
 
   const userName = readlineSync.question("May I have your name? ");
+  if (!userName) {
+    console.log("User Name field couldn't be empty");
+    return false;
+  }
   console.log(`Hello, ${userName}!\n`);
 
   while (iter < 3) {
     answer = readlineSync.question(`Question: ${numbers[iter]}\nYour answer: `);
+    if (!isEven(numbers[iter]) && answer === "yes") {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${userName}!`);
+      return false;
+    }
+    if (isEven(numbers[iter]) && answer === "no") {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, ${userName}!`);
+      return false;
+    }
     if (
       (isEven(numbers[iter]) && answer === "yes") || (!isEven(numbers[iter]) && answer === "no")
     ) {
       console.log("Correct!");
+    } else {
+      console.log(`'${answer}', is not correct answer.\n${userName}, please use keyword 'yes' or 'no'.`);
+      return false;
     }
-
-    if (!isEven(numbers[iter]) && answer === "yes") {
-      return console.log(`'${answer}' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${userName}!`);
-    }
-    if (isEven(numbers[iter]) && answer === "no") {
-      return console.log(`'${answer}' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, ${userName}!`);
-    }
-    // return `${answer}, is not correct answer.\n ${userName}, please use keyword 'yes' or 'no'.`;
     iter += 1;
   }
-
   return console.log(`Congratulations, ${userName}!`);
 };
 
