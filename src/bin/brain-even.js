9516ecb85c7c37fe59deb5db321e1e5b599879b6
Denis Grabina <denisgrabina@gmail.com>
2019-12-04 17:01:42 +0200

    refactor cotroller, reduce lines

--- src/bin/brain-even.js
+++ src/bin/brain-even.js
@@ -4,6 +4,8 @@ import greetingUser from "../components/greetingUser";
 import showRules from "../brain-even/showRules";
 import controller from "../brain-even/controller";
 
+const numbers = [15, 6, 7];
+
 greetingUser();
 showRules();
-controller();
+controller(numbers);
--- src/brain-even/controller.js
+++ src/brain-even/controller.js
@@ -1,40 +1,25 @@
 import readlineSync from "readline-sync";
 import isEven from "./isEven";
 
-const controller = () => {
+export default (numbers) => {
   let iter = 0;
-  let answer = "";
-  const numbers = [15, 6, 7];
 
   const userName = readlineSync.question("May I have your name? ");
-  if (!userName) {
-    console.log("User Name field couldn't be empty");
-    return false;
-  }
+  if (!userName) return console.log("User Name field couldn't be empty");
   console.log(`Hello, ${userName}!\n`);
 
   while (iter < 3) {
-    answer = readlineSync.question(`Question: ${numbers[iter]}\nYour answer: `);
-    if (!isEven(numbers[iter]) && answer === "yes") {
-      console.log(`'${answer}' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${userName}!`);
-      return false;
-    }
-    if (isEven(numbers[iter]) && answer === "no") {
-      console.log(`'${answer}' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, ${userName}!`);
-      return false;
-    }
-    if (
-      (isEven(numbers[iter]) && answer === "yes") || (!isEven(numbers[iter]) && answer === "no")
-    ) {
+    const answer = readlineSync.question(`Question: ${numbers[iter]}\nYour answer: `);
+    if (!isEven(numbers[iter]) && answer === "yes") return console.log(`'${answer}' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${userName}!`);
+
+    if (isEven(numbers[iter]) && answer === "no") return console.log(`'${answer}' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, ${userName}!`);
+
+    if ((isEven(numbers[iter]) && answer === "yes") || (!isEven(numbers[iter]) && answer === "no")) {
       console.log("Correct!");
     } else {
-      console.log(`'${answer}', is not correct answer.\n${userName}, please use keyword 'yes' or 'no'.`);
-      return false;
+      return console.log(`'${answer}', is not correct answer.\n${userName}, please use keyword 'yes' or 'no'.`);
     }
     iter += 1;
   }
-  console.log(`Congratulations, ${userName}!`);
-  return false;
+  return console.log(`Congratulations, ${userName}!`);
 };
-
-export default controller;
