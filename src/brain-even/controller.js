5077e77329d5fe2e46865996002b7e8e49b64614
Denis Grabina <denisgrabina@gmail.com>
2019-12-04 17:32:56 +0200

    refactor controller, change while to for loop, reduce more lines

--- src/brain-even/controller.js
+++ src/brain-even/controller.js
@@ -2,16 +2,13 @@ import readlineSync from "readline-sync";
 import isEven from "./isEven";
 
 export default (numbers) => {
-  let iter = 0;
-
   const userName = readlineSync.question("May I have your name? ");
   if (!userName) return console.log("User Name field couldn't be empty");
   console.log(`Hello, ${userName}!\n`);
 
-  while (iter < 3) {
+  for (let iter = 0; iter < 3; iter += 1) {
     const answer = readlineSync.question(`Question: ${numbers[iter]}\nYour answer: `);
     if (!isEven(numbers[iter]) && answer === "yes") return console.log(`'${answer}' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${userName}!`);
-
     if (isEven(numbers[iter]) && answer === "no") return console.log(`'${answer}' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, ${userName}!`);
 
     if ((isEven(numbers[iter]) && answer === "yes") || (!isEven(numbers[iter]) && answer === "no")) {
@@ -19,7 +16,6 @@ export default (numbers) => {
     } else {
       return console.log(`'${answer}', is not correct answer.\n${userName}, please use keyword 'yes' or 'no'.`);
     }
-    iter += 1;
   }
   return console.log(`Congratulations, ${userName}!`);
 };
