bcbeb6349e440cb9c7c35808b607c9f8162be134
Denis Grabina <denisgrabina@gmail.com>
2019-12-19 17:10:10 +0200

    add path for linter

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -4,11 +4,11 @@ import getRandomInt from "../components/getRandomInt";
 import controller from "../controllers/gameLogic";
 
 export default () => {
-  const rules = getRules("What number is missing in the progression?\n");
-  const data = [];
-  for (let i = 0; i < 3; i += 1) {
-    data.push(progression(getRandomInt(10)));
-  }
+	const rules = getRules("What number is missing in the progression?\n");
+	const data = [];
+	for (let i = 0; i < 3; i += 1) {
+		data.push(progression(getRandomInt(10)));
+	}
 
-  return controller(rules, data);
+	return controller(rules, data);
 };
