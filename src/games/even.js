f57b10d7d72fa3272d97cb117259ceea42dc80b7
Denis Grabina <denisgrabina@gmail.com>
2020-02-05 23:29:21 +0200

    change even js

--- src/games/even.js
+++ src/games/even.js
@@ -1,18 +1,14 @@
 import getRandomInt from "../getRandomInt";
 import controller from "..";
 
-const isEven = n => {
-	const expression = n;
-	const answer = n % 2 === 0 ? "yes" : "no";
-
-	return { expression, answer };
-};
+const isEven = n => n % 2 ? true : false;
+const result = expression => isEven(expression) ? "yes" : "no";
 
 export default () => {
 	const rules = "Answer 'yes' if number even otherwise answer 'no'.\n";
 	const data = [];
 	for (let i = 0; i < 3; i += 1) {
-		data.push(isEven(getRandomInt(100)));
+		data.push(result(isEven(getRandomInt(100))));
 	}
 
 	return controller(rules, data);
