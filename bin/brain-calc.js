fab1c7cbe3318d0f990eab36c7e7b6a1fe116675
Denis Grabina <denisgrabina@gmail.com>
2021-08-17 22:32:04 +0300

    fix build bin files

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,5 +1,2 @@
 #!/usr/bin/env node
-import { runGameEngine } from '../src/cli.js';
 import game from '../src/games/calc.js';
-
-runGameEngine(game);
--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -1,5 +1,2 @@
 #!/usr/bin/env node
-import { runGameEngine } from '../src.js';
 import game from '../src/games/prime.js';
-
-runGameEngine(game);
